75ea7353c018f9ee708d46f261119826d7483ae7
Jan Raadts <janraadts@outlook.de>
2022-12-14 15:52:38 +0000

    creating marker on the map, with the map array method

--- components/Map.js
+++ components/Map.js
@@ -1,13 +1,28 @@
-import { MapContainer, TileLayer, ZoomControl } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  ZoomControl,
+  Marker,
+  Popup,
+} from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import styled from "styled-components";
 
-export default function Map() {
+const markerIcon = L.divIcon({
+  html: `<svg width="48px" height="48px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="var(--primary-black)"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>`,
+  className: "",
+  iconSize: [48, 48],
+  iconAnchor: [24, 48],
+  popupAnchor: [0, -48],
+});
+
+export default function Map({ surfspots }) {
   return (
     <>
       <StyledMapContainer
-        center={[27.984865, -15.377071]}
-        zoom={11}
+        center={[54.452217, 11.069011]}
+        zoom={9}
         scrollWheelZoom={false}
         zoomControl={false}
       >
@@ -15,7 +30,20 @@ export default function Map() {
           url={`https://api.mapbox.com/styles/v1/jarakle/clbmsc8mz000w14o4oil6s70z/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamFyYWtsZSIsImEiOiJjbGJtbzlsYXkwNnY3M29yeDZhOGFsZW15In0.RFqqOxiya31Sjc70F1fmFg`}
           attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
         />
+
         <ZoomControl position="bottomright" />
+        {surfspots.map((surfspot) => {
+          return (
+            <Marker
+              position={[surfspot.latitude, surfspot.longitude]}
+              key={surfspot.ID}
+              ID={surfspot.ID}
+              icon={markerIcon}
+            >
+              <Popup>Coming soon...</Popup>
+            </Marker>
+          );
+        })}
       </StyledMapContainer>
     </>
   );
