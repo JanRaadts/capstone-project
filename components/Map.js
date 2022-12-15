126a5a6256c1342dae3e5347e24c7edea6f62bff
Jan Raadts <janraadts@outlook.de>
2022-12-15 16:26:11 +0000

    changed map design

--- components/Map.js
+++ components/Map.js
@@ -24,7 +24,7 @@ export default function Map({ surfspots }) {
         zoomControl={false}
       >
         <TileLayer
-          url={`https://api.mapbox.com/styles/v1/jarakle/clbmtjmr7000p14pgicyk8q8k/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamFyYWtsZSIsImEiOiJjbGJtbzlsYXkwNnY3M29yeDZhOGFsZW15In0.RFqqOxiya31Sjc70F1fmFg`}
+          url={`https://api.mapbox.com/styles/v1/jarakle/clbpaiuf5000414s3tgklaqtr/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamFyYWtsZSIsImEiOiJjbGJtbzlsYXkwNnY3M29yeDZhOGFsZW15In0.RFqqOxiya31Sjc70F1fmFg`}
           attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
         />
 
