3f5550377c27435311770d8dcb3d98a5631c8d33
Jan Raadts <janraadts@outlook.de>
2023-01-07 22:49:37 +0000

    changed the localstorage gook

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,26 @@ import GlobalStyles from "../components/GlobalStyles";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useLocalStorage } from "../components/Hooks/useLocalStorage";
+import useLocalStorageState from "use-local-storage-state";
 
 function MyApp({ Component, pageProps }) {
-  const [center, setCenter] = useLocalStorage("center", [54.434051, 10.318242]);
-  const [zoom, changeZoom] = useLocalStorage("zoom", 11);
-  const [favoriteSpots, setFavoriteSpots] = useLocalStorage(
+  // const [center, setCenter] = useLocalStorage("center", [54.434051, 10.318242]);
+  const [center, setCenter] = useLocalStorageState("center", {
+    defaultValue: [[54.434051, 10.318242]],
+  });
+  // const [zoom, changeZoom] = useLocalStorage("zoom", 11);
+  const [zoom, setZoom] = useLocalStorageState("zoom", {
+    defaultValue: [11],
+  });
+  // const [favoriteSpots, setFavoriteSpots] = useLocalStorage(
+  //   "favoriteSpots",
+  //   []
+  // );
+  const [favoriteSpots, setFavoriteSpots] = useLocalStorageState(
     "favoriteSpots",
-    []
+    {
+      defaultValue: [[]],
+    }
   );
 
   const [surfspots, setSurfspots] = useState([]);
@@ -35,7 +48,7 @@ function MyApp({ Component, pageProps }) {
         center={center}
         changeCenter={setCenter}
         zoom={zoom}
-        changeZoom={changeZoom}
+        changeZoom={setZoom}
         favoriteSpots={favoriteSpots}
         setFavoriteSpots={setFavoriteSpots}
         listFavSpots={listFavSpots}
