74e16756ff97a3b73240d8c875c4af9981be2d7e
Jan Raadts <janraadts@outlook.de>
2023-01-11 18:19:46 +0000

    added insta provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,6 +8,10 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    InstagramProvider({
+      clientId: process.env.INSTAGRAM_CLIENT_ID,
+      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+    }),
     // ...add more providers here
   ],
 };
